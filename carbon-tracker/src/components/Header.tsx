a844869d923cd2ad9464a13aed5ed59db604b80f
LukeJwalsh <98495526+LukeJwalsh@users.noreply.github.com>
2025-03-16 22:54:42 -0400

    Update Header.tsx

--- carbon-tracker/src/components/Header.tsx
+++ carbon-tracker/src/components/Header.tsx
@@ -14,19 +14,12 @@ const Header: React.FC<HeaderProps> = ({ title }) => {
   return (
     <header className="bg-[#145a32] text-white p-4 flex items-center justify-between fixed top-0 left-0 w-full z-50 shadow-[0_6px_12px_rgba(0,0,0,0.5)]">
       <div className="flex items-center flex-wrap">
-        <h1 className="text-xl font-bold mr-4">{title} 🌱</h1>
+        <h1 className="text-3xl font-bold mr-4">{title} 🌱</h1>
         <nav className="flex flex-wrap">
-          {/* Apply the glow-on-hover class here */}
-          <Link
-            to="/"
-            className="mr-4 text-white hover: glow-on-hover"
-          >
+          <Link to="/" className="mr-4 text-white hover:underline glow-on-hover">
             📓Activity Log
           </Link>
-          <Link
-            to="/summary"
-            className="text-white hover: glow-on-hover"
-          >
+          <Link to="/summary" className="text-white hover:underline glow-on-hover">
             📊Summary
           </Link>
         </nav>
